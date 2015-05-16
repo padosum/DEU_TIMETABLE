6d97fc752e2a9d0c41f9ee0a9bacc84941b6b2be
Yeonjeong Choi <makalu171@gmail.com>
2015-05-16 22:39:18 +0900

    Update DeuTimeTable.java
    GUI change

--- DEU-TIMETABLE/src/gui/main/DeuTimeTable.java
+++ DEU-TIMETABLE/src/gui/main/DeuTimeTable.java
@@ -1,55 +1,61 @@
 package gui.main;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Label;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Enumeration;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
 import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.UIManager; 	
-import javax.swing.plaf.FontUIResource;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
+import javax.swing.JFileChooser;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.JLabel;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import gui.main.AddMenu;
-import gui.main.main_layout;
 import data.DefineString;
 
-public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
-	/**
-	 * 
-	 */
+public class DeuTimeTable extends JFrame implements ActionListener {
+
+	private JPanel contentPane;
+	private JTable table, table_add, timetable;
+
+	private JComboBox comboBox;
+	private JTextField searchField;
+	
 	private static final long serialVersionUID = 1L;
 	Label lbl1, lbl2, lbl3;
 	static int row,col=0;
 	static int bottomRow =0;
 	Object object, scheObject = "";
-	JTable table, table_add, timetable;
 	private String bottomData[][];
 	private String[] colName = {DefineString.Parser.SORT, DefineString.Parser.LECTURE_NUM, DefineString.Parser.LECTURE_NAME, DefineString.Parser.CREDIT, 
 			DefineString.Parser.TIME, DefineString.Parser.GRADE, DefineString.Parser.PROFESSOR, DefineString.Parser.LECTURE_ROOM};
@@ -68,24 +74,54 @@ public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
            {"18:00 ~ 18:50", "", "", "", "", "", "", ""},
            {"19:00 ~ 19:50", "", "", "", "", "", "", ""}
            };
+	  
+	   
+	   
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeuTimeTable frame = new DeuTimeTable();
+					frame.setVisible(true);
+			        
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 	
 	
-	public void top(final JFrame frame) {
-		// 메뉴바 객체 생성
+	/**
+	 * Create the frame.
+	 */
+	public DeuTimeTable() {
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 750);
+		setTitle(DefineString.TITLE);
+		
+		setLocationRelativeTo(null); // 프레임을모니터화면중앙에배치
+		
+		// 메뉴바 생성, 프레임에 추가
 		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 		
 		// 메뉴 추가
 		JMenu menu = new JMenu(DefineString.Menu.FILE);
 		JMenu help = new JMenu(DefineString.Menu.HELP);
-		
-		
+				
+				
 		// 서브 메뉴 생성
 		JMenuItem show = new JMenuItem(DefineString.Menu.SHOW_TIME_TABLE);
 		JMenuItem save = new JMenuItem(DefineString.Menu.SAVE_TIME_TABLE);
 		JMenuItem load = new JMenuItem(DefineString.Menu.LOAD_TIME_TABLE);
 		JMenuItem load_lecture = new JMenuItem(DefineString.Menu.LOAD_HAND_BOOK);
 	    JMenuItem curriculum = new JMenuItem(DefineString.Menu.LOAD_CURRICULUM);
-		
 		JMenuItem about = new JMenuItem(DefineString.AboutThis.TITLE);
 
 		// 서브 메뉴 추가
@@ -94,71 +130,63 @@ public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
 		menu.add(load);
 		menu.addSeparator();	// 분리선 추가
 		menu.add(load_lecture);
-	    menu.add(curriculum);
+		menu.add(curriculum);
 		help.add(about);
-		
+				
 		// 메뉴바에 메뉴 추가
 		menuBar.add(menu);
 		menuBar.add(help);
 		
-		// 프레임에 메뉴바 추가
-		frame.setJMenuBar(menuBar);
-		
-	      //메뉴 기능 추가
-	      load_lecture.addActionListener(new ActionListener() {
-	        
-	        public void actionPerformed(ActionEvent arg0) {
-	            try                                      
-	             { 
-	               //강의편람 열기
-	                 Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + "201502_sugang_handbook.hwp");
-
+		// 메뉴 기능 추가
+	    load_lecture.addActionListener(new ActionListener() {
+		        
+	    public void actionPerformed(ActionEvent arg0) {
+		          try                                      
+		           { 
+		             //강의편람 열기
+		               Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + "201502_sugang_handbook.hwp");
 	             } catch (Exception e)                   
-	               { 
+                   { 
 	                   System.out.println(DefineString.NO_FILE);  
 	               } 
-	         } 
-	                   
+		         } 
+		                   
 	     });
-	      
-	      curriculum.addActionListener(new ActionListener() {
-	      
-	      @Override
-	      public void actionPerformed(ActionEvent arg0) {
-	         // TODO Auto-generated method stub
-	          try                                      
-	                { 
-	                  //강의편람 열기
-	                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + "curriculum.pdf");
-
-	                } catch (Exception e)                   
-	                  { 
-	                      System.out.println(DefineString.NO_FILE);  
-	                  } 
-	            } 
-	   });
 		
-		//jcombobox 객체 추가
-		JComboBox<String> jc = new JComboBox<String>();
-		add(jc);
-		jc.addItem(DefineString.Parser.SORT);
-		jc.addItem(DefineString.Parser.LECTURE_NAME);
-		jc.addItem(DefineString.Parser.CREDIT);
-		jc.addItem(DefineString.Parser.TIME);
-		jc.addItem(DefineString.Parser.GRADE);
-		jc.addItem(DefineString.Parser.PROFESSOR);
-		jc.addItem(DefineString.Parser.LECTURE_ROOM);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		//jtextfield 객체 추가
-		JTextField jt = new JTextField(10);
-		add(jt);
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
-		//jbutton 객체 추가
-		JButton search = new JButton(DefineString.SEARCH);
-		add(search);
-	} 
-
-	public void middle(final JFrame frame) {
+		// top 패널 
+		JPanel top = new JPanel();
+		panel.add(top);
+		
+		// jcomboBox 객체 추가
+		comboBox = new JComboBox(new Object[]{DefineString.Parser.SORT, DefineString.Parser.LECTURE_NAME, DefineString.Parser.CREDIT, 
+	    		DefineString.Parser.TIME, DefineString.Parser.GRADE, DefineString.Parser.PROFESSOR, DefineString.Parser.LECTURE_ROOM});
+		top.add(comboBox);
+		
+		// jtextField 객체 추가
+		searchField = new JTextField();
+		top.add(searchField);
+		searchField.setColumns(20);
+		
+		// 검색 버튼 추가
+		JButton button = new JButton("검색");
+		top.add(button);
+		
+		// middle 패널
+		JPanel middle = new JPanel();
+		panel.add(middle);
+		middle.setLayout(new FlowLayout(FlowLayout.CENTER, 3, 5));
+		
+		
+		
 		FileInputStream fis = null;
 		int rowindex=0;
 		int columnindex=0;
@@ -167,8 +195,6 @@ public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
 		 JFileChooser jfc = new JFileChooser();
 		 jfc.setMultiSelectionEnabled(true);
 	     jfc.showOpenDialog(null);
-	     File selectedFiles[] = jfc.getSelectedFiles(); // 파일 선택
-	     
 	     for(int i=0; i<jfc.getSelectedFiles().length; i++)
 	     {
 	    	 
@@ -262,105 +288,76 @@ public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
 			
 		}
 		// JTable생성자를 이용하여 테이블을 만든다.
-		table = new JTable(data2, colName);	
-
-		// JScrollPane에 테이블을 붙이고, 프레임에 붙인다.
-		add(new JScrollPane(table));
-
-		}
-	
-	public void middle2(JFrame frame) // 버튼 이벤트
-	{
+		//table = new JTable(data2, colName);	
+        
 		
+	    DefaultTableModel model = new DefaultTableModel(data2, colName);
+	    table = new JTable(model);
+	    
+		JScrollPane scrollPane = new JScrollPane();
+		middle.add(scrollPane);
+	    scrollPane.setViewportView(table);
+		
+	    
+	    // 추가 삭제 버튼 
 		JButton add = new JButton(DefineString.ADD);
-		add(add);
+		middle.add(add);
 		
 		JButton remove = new JButton(DefineString.REMOVE);
-		add(remove);
+		middle.add(remove);
 		
 		add.addActionListener(this);
 		remove.addActionListener(this);
 		
-
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
 		
+		JLabel label = new JLabel("");
+		middle.add(label);
 		
-		String button = e.toString();
-		row = table.getSelectedRow();
-		col = table.getSelectedColumn();
+		JPanel bottom = new JPanel();
+		panel.add(bottom);
 		
-
-		//System.out.println(bottomData2.length);
-		if (button.contains(DefineString.ADD))
-		{
-			
-			
-			for (int i = 0 ; i < table_add.getColumnCount() ; i++)
-			{
-				if(bottomRow < 0)
-					bottomRow =0;
-
-				object = table.getValueAt(row, i);
-				bottomData[bottomRow][i] = (String) object;
-				
-			}
-			dtm.addRow(bottomData[bottomRow]);
-			bottomRow++;
-			Schedule();
-
-			// JTable생성자를 이용하여 테
-		}
-		
-
-		else
-		{
-			try {
-		            dtm.removeRow(table_add.getSelectedRow());
-		        }
-		            
-		    catch (Exception e2) {
-		        JOptionPane.showMessageDialog(this, DefineString.WARNING);
-
-		        
-		         
-		 }
-			bottomRow--;
-			
-		}
-
-		
-		table_add.updateUI();
-		timetable.updateUI();
-		
-	}
-	
-	
-	
-	public void bottom(final JFrame frame) {
+		JScrollPane scrollPane_1 = new JScrollPane();
+		bottom.add(scrollPane_1);
+		scrollPane_1.setPreferredSize(new Dimension(450, 100));
 		
 		bottomData = new String[20][8];
 		
-
-		// 테이블의 열 이름이 들어갈 내용을 일차원 배열에 넣는다.
-		String[] colName = {DefineString.Parser.SORT, DefineString.Parser.LECTURE_NUM, DefineString.Parser.LECTURE_NAME, DefineString.Parser.CREDIT, 
-				DefineString.Parser.TIME, DefineString.Parser.GRADE, DefineString.Parser.PROFESSOR, DefineString.Parser.LECTURE_ROOM};
+		table_add = new JTable(dtm);
+		scrollPane_1.setViewportView(table_add);
 		
-		// JTable생성자를 이용하여 테이블을 만든다.
-		table_add = new JTable(dtm);	
-		// JScrollPane에 테이블을 붙이고, 프레
-		JScrollPane scroll = new JScrollPane(table_add);
-		scroll.setPreferredSize(new Dimension(450, 100)); // scroll 크기
-		// table.setPreferredSize(new Dimension(450, 500));
-		add(scroll);
+		JLabel label_1 = new JLabel("");
+		panel.add(label_1);
 		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1);
+		panel_1.setLayout(new BorderLayout(0, 0));
 		
-	
+		JScrollPane timeweek = new JScrollPane();
+		panel_1.add(timeweek);
+		
+		String []weekday = {DefineString.Week.TIME, DefineString.Week.MON, DefineString.Week.TUE, DefineString.Week.WEN, DefineString.Week.THU, DefineString.Week.FRI};
+		timetable = new JTable(time, weekday);
+	    // 행 세로 길이
+	    timetable.setRowHeight(50);
+	    // 총 길이 500에 퍼센테이지로 지정
+	    setJTableColumnsWidth(timetable, 500, 20, 16, 16, 16, 16, 16);
+	    
+        // 중앙정렬
+        DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+              dtcr.setHorizontalAlignment(SwingConstants.CENTER);
+              TableColumnModel tcm = timetable.getColumnModel();
+              for(int i = 0 ; i< tcm.getColumnCount() ; i++)
+              {
+               tcm.getColumn(i).setCellRenderer(dtcr);
+              }
+	    
+	    Color a = new Color(255, 255, 255);
+		timeweek.setViewportView(timetable);
+	    timeweek.setBackground(a);
+	    timeweek.setPreferredSize(new Dimension(500, 200));
+	    
+	    
 	}
-	
     // 열 너비 지정 메소드 (퍼센테이지로 지정)
 	public static void setJTableColumnsWidth(JTable table, int tablePreferredWidth,
 	        double... percentages) {
@@ -376,38 +373,6 @@ public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
 	    }
 	}
 	
-	public void timeTable(final JFrame frame) {
-	      String []weekday = {DefineString.Week.TIME, DefineString.Week.MON, DefineString.Week.TUE, DefineString.Week.WEN, DefineString.Week.THU, DefineString.Week.FRI};
-	      timetable= new JTable(time, weekday);
-	      
-	      // 행 세로 길이
-	      timetable.setRowHeight(45);
-	      // 총 길이 500에 퍼센테이지로 지정
-	      setJTableColumnsWidth(timetable, 500, 20, 16, 16, 16, 16, 16);
-	      
-	      JScrollPane timeweek = new JScrollPane(timetable);
-	      Color a = new Color(255, 255, 255);
-	      timeweek.setBackground(a);
-	      timeweek.setPreferredSize(new Dimension(500, 200));//153 거의 맞음
-	      add(timeweek);
-	      
-	   }
-	
-	
-	// 폰트 설정 method
-	public static void setUIFont(FontUIResource f) {
-        Enumeration keys = UIManager.getDefaults().keys();
-        while (keys.hasMoreElements()) {
-            Object key = keys.nextElement();
-            Object value = UIManager.get(key);
-            if (value instanceof FontUIResource) {
-                FontUIResource orig = (FontUIResource) value;
-                Font font = new Font(f.getFontName(), orig.getStyle(), f.getSize());
-                UIManager.put(key, new FontUIResource(font));
-            }
-        }
-    }
-	
 	private void Schedule()
 	{
         object = table.getValueAt(row, 7);
@@ -416,7 +381,6 @@ public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
         StringBuffer trans1 = new StringBuffer(trans);
         trans1.reverse();
         String retrans = trans1.toString();
-        int day = 0;
         String print = "";
         String backprint = "";
         int i1 = trans.indexOf('[');
@@ -597,35 +561,60 @@ public class DeuTimeTable extends JPanel implements AddMenu, ActionListener{
 	            }
 	            
 	            
-	            //중앙정렬
-	            DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
-	                  dtcr.setHorizontalAlignment(SwingConstants.CENTER);
-	                  TableColumnModel tcm = timetable.getColumnModel();
-	                  for(int i = 0 ; i< tcm.getColumnCount() ; i++)
-	                  {
-	                   tcm.getColumn(i).setCellRenderer(dtcr);
-	                  }
+
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String button = e.toString();
+		row = table.getSelectedRow();
+		col = table.getSelectedColumn();
+		
+		
+		if (button.contains(DefineString.ADD))
+		{
+			
+			
+			for (int i = 0 ; i < table_add.getColumnCount() ; i++)
+			{
+				if(bottomRow < 0)
+					bottomRow =0;
+
+				object = table.getValueAt(row, i);
+				bottomData[bottomRow][i] = (String) object;
+				
+			}
+			dtm.addRow(bottomData[bottomRow]);
+			bottomRow++;
+			Schedule();
+
+			// JTable생성자를 이용하여 테
+		}
+		
+
+		else
+		{
+			try {
+		            dtm.removeRow(table_add.getSelectedRow());
+		        }
+		            
+		    catch (Exception e2) {
+		        JOptionPane.showMessageDialog(this, DefineString.WARNING);
+
+		        
+		         
+		 }
+			bottomRow--;
+			
+		}
+
+		
+		table_add.updateUI();
+		timetable.updateUI();
+		
+	}
+
 	
-	public static void main(String[] args) {
-		final JPanel c = new DeuTimeTable();
-		c.setPreferredSize(new Dimension(520,850)); // 윈도우 창 크기
-		
-		// 폰트 설정
-		 setUIFont(new FontUIResource(new Font("나눔고딕", 0, 13)));
-		// EDT를 사용해 실행할 GUI 작업을 넣는다.
-		// title과 component
-		main_layout.launch(DefineString.TITLE, c);
-		
-		return;
-		
-	} 
+	
 
 }
-
-
-
-
-
-
-
