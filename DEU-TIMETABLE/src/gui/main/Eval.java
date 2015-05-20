d8f2c8a26348db3d54f7d5ff8a235c20071a7e0b
the1994 <makalu171@gmail.com>
2015-05-20 13:35:13 +0900

    import 정리

--- DEU-TIMETABLE/src/gui/main/Eval.java
+++ DEU-TIMETABLE/src/gui/main/Eval.java
@@ -1,32 +1,17 @@
 package gui.main;
 
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream.GetField;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
 
-import javax.swing.JButton;
+
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import org.apache.poi.ss.formula.ptg.AddPtg;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.helper.HttpConnection.Response;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
